93fcf59f322ba655f999f9711287404cceec1e5a
Arnab Biswas <khanp4397@gmail.com>
2026-03-13 23:27:03 +0530

    chore: update copyright year to 2026

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="border-t border-border py-8 bg-background">
       <div className="max-w-6xl mx-auto px-6 flex items-center justify-center">
-        <p className="text-dim-fg text-xs">Arnab Biswas &copy; 2025</p>
+        <p className="text-dim-fg text-xs">Arnab Biswas &copy; 2026</p>
       </div>
     </footer>
   );
